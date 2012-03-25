958ed37f1c825ae81a96a649017085a1ef2b8cf5
shadyabhi <abhijeet.1989@gmail.com>
2012-03-26 00:32:12 +0530

    Improved code. No new feature

--- addbutton.js
+++ addbutton.js
@@ -3,39 +3,46 @@ Author: Abhijeet Rastogi (shadyabhi)
 Email: abhijeet.1989@gmail.com
 */
 
-function runAgain(){
+LoadAllComments = {
+    runAgain : function () {
     //Run every one second
-    window.setTimeout(addButton,1e3)
-    }
+    window.setTimeout(LoadAllComments.clickFirstButton,1000);
+    },
     
-function addButton(){
-    //All buttons have class as "button"
-    var first_button = document.getElementsByClassName("button")[0];
-    if (first_button == null){
-        alert("Sorry, no comments to expand");
-        return;
-    }
+    clickFirstButton : function () {
+        //All buttons have class as "button"
+        var first_button = document.getElementsByClassName("button")[0];
+        if (first_button == null) {
+            alert("Sorry, no comments to expand");
+            return;
+        }
 
-    if (first_button.innerHTML == "loading..."){
-        first_button.className = ""; //To fix later
-    }
-    else{
-       var theEvent = document.createEvent("MouseEvent");
-       theEvent.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-       first_button.dispatchEvent(theEvent);
-    }
+        if (first_button.innerHTML == "loading...") {
+            first_button.className = ""; //To fix later
+        }
+        else{
+           var theEvent = document.createEvent("MouseEvent");
+           theEvent.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+           first_button.dispatchEvent(theEvent);
+        }
 
-    if(first_button.id) 
-        runAgain();
-    };
+        if (first_button.id) {
+            LoadAllComments.runAgain ();
+        }
+        },
 
-//Link created and added as child to "pane"
-var pane = document.getElementsByClassName("panestack-title")[0];
-var newlink = document.createElement("a");
-newlink.appendChild(document.createTextNode("Load all comments"));
-newlink.id = "loadmorecomments";
-newlink.href = "javascript:void(0)";
-newlink.className="title-button ";
-newlink.title = 'Opens all "load more comments" link every 1 second';
-newlink.addEventListener('click', runAgain, false);
-pane.appendChild(newlink);
+    createButton : function () {
+        //Link created and added as child to "pane"
+        var pane = document.getElementsByClassName("panestack-title")[0];
+        var newlink = document.createElement("a");
+        newlink.appendChild(document.createTextNode("Load all comments"));
+        newlink.id = "loadmorecomments";
+        newlink.href = "javascript:void(0)";
+        newlink.className = "title-button ";
+        newlink.title = 'Opens all "load more comments" link every 1 second';
+        newlink.addEventListener('click', LoadAllComments.runAgain, false);
+        pane.appendChild(newlink);
+    },
+};
+
+LoadAllComments.createButton();
